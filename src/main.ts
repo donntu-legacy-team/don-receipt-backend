ffcd5723e1c9d682883aebc9f080036e68f8eabf
DimaReshe <roramip132@nokdot.com>
2025-04-05 15:25:03 +0300

    DONRECEIPT-23 fix: CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:3002'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('DonReceipt api')
