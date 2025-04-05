8c6baca041ba0dda5c7f11d0185ff9396caf94f7
audworth <antonprklm@gmail.com>
2025-04-05 22:41:33 +0300

    DONRECEIPT-14-Categories-Admin feat: add update endpoint for subcategories

--- src/application/subcategories/subcategories.service.ts
+++ src/application/subcategories/subcategories.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Subcategory } from '@/domain/subcategories/subcategory.entity';
 import { Repository } from 'typeorm';
-import { CreateSubcategoryParams } from '@/application/subcategories/subcategories.types';
+import {
+  CreateSubcategoryParams,
+  UpdateSubcategoryParams,
+} from '@/application/subcategories/subcategories.types';
 
 @Injectable()
 export class SubcategoriesService {
@@ -24,6 +27,21 @@ export class SubcategoriesService {
       name: createSubcategoryParams.name,
       parentCategory: createSubcategoryParams.parentCategory,
     });
+    await this.subcategoriesRepository.save(subcategory);
+
+    return subcategory;
+  }
+
+  async updateSubcategory(updateSubcategoryParams: UpdateSubcategoryParams) {
+    const subcategory = await this.subcategoriesRepository.findOneBy({
+      id: updateSubcategoryParams.id,
+    });
+
+    if (!subcategory) {
+      return null;
+    }
+
+    subcategory.name = updateSubcategoryParams.name;
     await this.subcategoriesRepository.save(subcategory);
 
     return subcategory;
