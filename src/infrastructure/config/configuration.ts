0a8719e8bec9ac8b26361356b3cbd03e0147be4b
TarasPinchuk <kkomforr@yandex.ru>
2025-04-13 22:35:22 +0300

    DONRECEIPT-5-Authorization fix: ports in envdev

--- src/infrastructure/config/configuration.ts
+++ src/infrastructure/config/configuration.ts
@@ -38,16 +38,14 @@ export const config = (): Configuration => {
     env: isDev ? 'dev' : 'prod',
     http: {
       host: process.env.HTTP_HOST ?? 'localhost',
-      port: process.env.HTTP_PORT
-        ? parseInt(process.env.HTTP_PORT, 10)
-        : 3000,
+      port: process.env.HTTP_PORT ? parseInt(process.env.HTTP_PORT, 10) : 3000,
     },
     database: {
       host: process.env.POSTGRES_HOST ?? 'localhost',
       port: process.env.POSTGRES_PORT
         ? parseInt(process.env.POSTGRES_PORT, 10)
-        : 5432,
-      username: process.env.POSTGRES_USERNAME ?? 'postgres',
+        : 5433,
+      username: process.env.POSTGRES_USERNAME ?? 'root',
       password: process.env.POSTGRES_PASSWORD ?? 'root',
       databaseName: process.env.POSTGRES_DATABASE ?? 'don_receipt',
       synchronize: isDev,
@@ -57,12 +55,10 @@ export const config = (): Configuration => {
         ? parseInt(process.env.BCRYPT_SALT_ROUNDS, 10)
         : 10,
       jwtAccessSecret:
-        process.env.JWT_ACCESS_SECRET ||
-        'DO_NOT_USE_THIS_SECRET',
+        process.env.JWT_ACCESS_SECRET || 'DO_NOT_USE_THIS_SECRET',
       jwtAccessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN || '2h',
       jwtRefreshSecret:
-        process.env.JWT_REFRESH_SECRET ||
-        'DO_NOT_USE_THIS_SECRET',
+        process.env.JWT_REFRESH_SECRET || 'DO_NOT_USE_THIS_SECRET',
       jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '14d',
     },
   };
