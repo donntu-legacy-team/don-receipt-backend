04c5d4d0f9823721d2cdfddd2888f716d77487df
TarasPinchuk <kkomforr@yandex.ru>
2025-04-15 23:51:10 +0300

    DONRECEIPT-35 fix: added comment

--- src/interfaces/guards/jwt-auth.guard.ts
+++ src/interfaces/guards/jwt-auth.guard.ts
@@ -3,6 +3,7 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  // костыль, такой интерфейс у AuthGuard в @nestjs/passport
   handleRequest(err: any, user: any): any {
     if (err || !user) {
       throw err || new UnauthorizedException('Invalid access token');
