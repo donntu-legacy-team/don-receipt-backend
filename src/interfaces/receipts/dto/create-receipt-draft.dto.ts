922fd2c6c2ad2ab09d218c379edbb92ad05dbd21
[M] Pinchuk Taras <kkomforr@yandex.ru>
2025-05-20 21:45:50 +0300

    Decorators hotfix

--- src/interfaces/receipts/dto/create-receipt-draft.dto.ts
+++ src/interfaces/receipts/dto/create-receipt-draft.dto.ts
@@ -1,4 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsOptional } from 'class-validator';
+
 export class CreateReceiptDraftDto {
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
   title?: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
   receiptContent?: string;
 }
